26a3aa536d6c249f77f872ef0f2769d41f750e1b
MasterFeizz <masterfeizz@gmail.com>
2015-10-21 10:25:38 -0400

    Fix case-sensitive include

--- source/in_ctr.c
+++ source/in_ctr.c
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "quakedef.h"
 
-#include <3DS.h>
+#include <3ds.h>
 #include "ctr.h"
 
 //Touchscreen mode identifiers
